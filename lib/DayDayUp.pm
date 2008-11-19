69c85151611684a51f7254a5c03bc61484f356a6
Fayland 林 <fayland@gmail.com>
2008-11-19 03:37:07 +0000

    Import of FAYLAND/DayDayUp-0.01 from CPAN.
    gitpan-cpan-distribution: DayDayUp
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         FAYLAND/DayDayUp-0.01.tar.gz
    gitpan-cpan-author:       FAYLAND
    gitpan-cpan-maturity:     released

---
+++ lib/DayDayUp.pm
@@ -0,0 +1,93 @@
+package DayDayUp;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+use base 'Mojolicious';
+
+use MojoX::Renderer::TT;
+use Template::Stash::XS;
+
+# This method will run for each request
+sub dispatch {
+    my ($self, $c) = @_;
+
+    # Try to find a static file
+    my $done = $self->static->dispatch($c);
+
+    # Use routes if we don't have a response code yet
+    $done = $self->routes->dispatch($c) unless $done;
+
+    # Nothing found, serve static file "public/404.html"
+    unless ($done) {
+        $self->static->serve($c, '/404.html');
+        $c->res->code(404);
+    }
+}
+
+# This method will run once at server start
+sub startup {
+    my $self = shift;
+
+    # Use our own context class
+    $self->ctx_class('DayDayUp::Context');
+
+    # Routes
+    my $r = $self->routes;
+
+    # route
+    $r->route('/notes/:id/:action', id => qr/\d+/)
+      ->to(controller => 'notes', action => 'index');
+    # Default route
+    $r->route('/:controller/:action')
+      ->to(controller => 'index', action => 'index');
+
+    my $tt = MojoX::Renderer::TT->build(
+        mojo => $self,
+        template_options => {
+            POST_CHOMP => 1,
+            PRE_CHOMP  => 1,
+            STASH      => Template::Stash::XS->new,
+            INCLUDE_PATH => [ $self->home->rel_dir('templates') ],
+            WRAPPER    => 'wrapper.html',
+        }
+    );
+    $self->renderer->add_handler( html => $tt );
+
+}
+
+1;
+__END__
+
+=head1 NAME
+
+DayDayUp - good good study, day day up
+
+=head1 DESCRIPTION
+
+it's just a test with L<Mojo>
+
+but I don't mind if you use it in your localhost.
+
+=head1 RUN
+
+	perl bin/day_day_up daemon
+
+=head1 CONFIGURATION
+
+create a daydayup_local.yml at the same dir as daydayup.yml
+
+=head1 AUTHOR
+
+Fayland Lam < fayland at gmail dot com >
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2008 Fayland Lam, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
---
+++ lib/DayDayUp/Notes.pm
@@ -0,0 +1,212 @@
+package DayDayUp::Notes;
+
+use strict;
+use warnings;
+
+use base 'Mojolicious::Controller';
+
+use Data::Dumper;
+
+use vars qw/%levels %status/;
+%levels = (
+    99 => 'A-1',
+    98 => 'A-2',
+    97 => 'A-3',
+    66 => 'B-1',
+    65 => 'B-2',
+    64 => 'B-3',
+    33 => 'C-1',
+    32 => 'C-2',
+    31 => 'C-3',
+);
+%status = (
+    0  => 'closed',
+    1  => 'suspended',
+    2  => 'open',
+);
+
+sub index {
+    my ($self, $c) = @_;
+    
+    my $config = $c->config;
+    my $dbh = $c->dbh;
+    
+    my $notes = $self->_get_notes( $c );
+    $c->render(template => 'notes/index.html', notes => $notes, levels => \%levels);
+}
+
+sub add {
+    my ( $self, $c ) = @_;
+    
+    my $stash = {
+        template => 'notes/add.html',
+        levels   => \%levels,
+    };
+    unless ( $c->req->method eq 'POST' ) {
+        return $c->render( $stash );
+    }
+    
+    my $config = $c->config;
+    my $dbh = $c->dbh;
+    my $params = $c->req->params->to_hash;
+    
+    my $notes = $params->{notes};
+    my $level = $params->{level};
+    my $sql = q~INSERT INTO notes ( note, level, status, time ) VALUES ( ?, ?, ?, ? )~;
+    my $sth = $dbh->prepare($sql);
+    $sth->execute( $notes, $level, 2, time() );
+    
+    $c->render(template => 'redirect.html', url => '/notes/');
+}
+
+sub _get_notes {
+    my ( $self, $c ) = @_;
+    
+    my $notes;
+    
+    my $dbh = $c->dbh;
+    
+    # open
+    my $sql = q~SELECT * FROM notes WHERE status = 2~; 
+    my $sth = $dbh->prepare($sql);
+    $sth->execute;
+    $notes->{open} = $sth->fetchall_arrayref({});
+    
+    # suspended
+    $sql = q~SELECT * FROM notes WHERE status = 1~; 
+    $sth = $dbh->prepare($sql);
+    $sth->execute;
+    $notes->{suspended} = $sth->fetchall_arrayref({});
+    
+    # closed
+    $sql = q~SELECT * FROM notes WHERE status = 0 ORDER BY time DESC LIMIT 10~; 
+    $sth = $dbh->prepare($sql);
+    $sth->execute;
+    $notes->{closed} = $sth->fetchall_arrayref({});
+    
+    return $notes;
+}
+
+sub edit {
+    my ( $self, $c ) = @_;
+    
+    my $captures = $c->match->captures;
+    my $id = $captures->{id};
+    
+    my $dbh = $c->dbh;
+    
+    # get the note
+    my $sql = q~SELECT * FROM notes WHERE note_id = ?~;
+    my $sth = $dbh->prepare($sql);
+    $sth->execute( $id );
+    my $note = $sth->fetchrow_hashref;
+    
+    my $stash = {
+        template => 'notes/add.html',
+        levels   => \%levels,
+    };
+    unless ( $c->req->method eq 'POST' ) {
+    	# pre-fulfill
+    	$stash->{fif} = {
+    		level => $note->{level},
+    		notes => $note->{note},
+    	};
+        return $c->render( $stash );
+    }
+    
+    my $params = $c->req->params->to_hash;
+    
+    my $notes = $params->{notes};
+    my $level = $params->{level};
+    $sql = q~UPDATE notes SET note = ?, level = ? WHERE note_id = ?~;
+    $sth = $dbh->prepare($sql);
+    $sth->execute( $notes, $level, $id );
+    
+    $c->render(template => 'redirect.html', url => '/notes/');
+}
+
+sub delete {
+    my ( $self, $c ) = @_;
+    
+    my $captures = $c->match->captures;
+    my $id = $captures->{id};
+    
+    my $dbh = $c->dbh;
+    my $sql = q~DELETE FROM notes WHERE note_id = ?~;
+    my $sth = $dbh->prepare($sql);
+    $sth->execute( $id );
+    
+    $c->render(template => 'redirect.html', url => '/notes/');
+}
+
+sub update {
+	my ( $self, $c ) = @_;
+	
+	my $captures = $c->match->captures;
+    my $id = $captures->{id};
+    
+    my $dbh = $c->dbh;
+    my $params = $c->req->params->to_hash;
+    
+    my $status = $params->{status};
+    my $st_val = 2;
+    foreach my $key ( keys %status ) {
+    	if ( $status{ $key } eq $status ) {
+    		$st_val = $key;
+    		last;
+    	}
+    }
+    
+    my $sql = q~UPDATE notes SET status = ? WHERE note_id = ?~;
+    my $sth = $dbh->prepare($sql);
+    $sth->execute( $st_val, $id );
+    
+    $c->render(template => 'redirect.html', url => '/notes/');
+}
+
+sub closed {
+	my ( $self, $c ) = @_;
+	
+	my $dbh = $c->dbh;
+	
+	my $sql = q~SELECT * FROM notes WHERE status = 0 ORDER BY time DESC~; 
+    my $sth = $dbh->prepare($sql);
+    $sth->execute;
+    my $closed = $sth->fetchall_arrayref({});
+    
+    $c->render(
+		template => 'notes/index.html',
+		notes => { closed => $closed },
+		is_in_closed_page => 1,
+		levels => \%levels
+	);
+}
+
+1;
+__END__
+
+=head1 NAME
+
+DayDayUp::Notes - Mojolicious::Controller, /notes/
+
+=head1 URL
+
+	/notes/
+	/notes/add
+	/notes/$id/edit
+	/notes/$id/delete
+	/notes/$id/update
+	/notes/closed
+
+=head1 AUTHOR
+
+Fayland Lam < fayland at gmail dot com >
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2008 Fayland Lam, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
