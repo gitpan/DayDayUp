14c54af5c86465fff8e7bff04933cc23e75f72c6
Fayland 林 <fayland@gmail.com>
2009-04-28 03:47:30 +0000

    Import of FAYLAND/DayDayUp-0.90 from CPAN.
    gitpan-cpan-distribution: DayDayUp
    gitpan-cpan-version:      0.90
    gitpan-cpan-path:         FAYLAND/DayDayUp-0.90.tar.gz
    gitpan-cpan-author:       FAYLAND
    gitpan-cpan-maturity:     released

--- lib/DayDayUp/Notes.pm
+++ lib/DayDayUp/Notes.pm
@@ -1,238 +1,161 @@
-package DayDayUp::Notes;
+use MooseX::Declare;
 
-use strict;
-use warnings;
-
-our $VERSION = '0.09';
-
-use base 'Mojolicious::Controller';
-
-use Data::Dumper;
-
-use vars qw/%levels %status/;
-%levels = (
-    99 => 'A-1',
-    98 => 'A-2',
-    97 => 'A-3',
-    66 => 'B-1',
-    65 => 'B-2',
-    64 => 'B-3',
-    33 => 'C-1',
-    32 => 'C-2',
-    31 => 'C-3',
-);
-%status = (
-    0  => 'closed',
-    1  => 'suspended',
-    2  => 'open',
-    3  => 'rejected',
-);
-
-sub index {
-    my ($self, $c) = @_;
+class DayDayUp::Notes extends Mojolicious::Controller is mutable {
+    use DayDayUpX::Note;
     
-    my $config = $c->config;
-    my $dbh = $c->dbh;
+    our $VERSION = '0.09';
     
-    my $notes = $self->_get_notes( $c );
-    $c->render(template => 'notes/index.html', notes => $notes, levels => \%levels);
-}
+    method index ($c) {
+        
+        my $notes;
+        
+        my $kioku = $c->kioku;
+        my $scope = $kioku->new_scope;
+        my $all = $kioku->backend->all_entries;
+        while( my $chunk = $all->next ){
+            entry: for my $id (@$chunk) {
+                my $entry = $kioku->lookup($id->id);
+                next entry unless blessed $entry && $entry->isa('DayDayUpX::Note');
+                $entry->{id} = $id->id; # hack
+                push @{ $notes->{ $entry->status} }, $entry;
+            }
+        }
+        
+        # sort by time DESC
+        foreach my $key ( keys %$notes ) {
+            $notes->{$key}
+                = [ sort { $b->time <=> $a->time } @{ $notes->{$key} } ];
+        }
 
-sub add {
-    my ( $self, $c ) = @_;
-    
-    my $stash = {
-        template => 'notes/add.html',
-        levels   => \%levels,
+        $c->render(template => 'notes/index.html', notes => $notes );
     };
-    unless ( $c->req->method eq 'POST' ) {
-        return $c->render( $stash );
-    }
     
-    my $config = $c->config;
-    my $dbh = $c->dbh;
-    my $params = $c->req->params->to_hash;
-    
-    my $notes = $params->{notes};
-    my $level = $params->{level};
-    my $sql = q~INSERT INTO notes ( note, level, status, time ) VALUES ( ?, ?, ?, ? )~;
-    my $sth = $dbh->prepare($sql);
-    $sth->execute( $notes, $level, 2, time() );
-    
-    $c->render(template => 'redirect.html', url => '/notes/');
-}
+    method add ($c) {
+        
+        my $stash = {
+            template => 'notes/add.html',
+        };
+        unless ( $c->req->method eq 'POST' ) {
+            return $c->render( $stash );
+        }
+        
+        my $config = $c->config;
+        my $params = $c->req->params->to_hash;
+        
+        my $note = DayDayUpX::Note->new(
+            text   => $params->{text},
+            status => 'open',
+            time   => time()
+        );
+        
+        my $scope = $c->kioku->new_scope;
+        $c->kioku->txn_do(sub {
+            $c->kioku->insert($note);
+        });
 
-sub _get_notes {
-    my ( $self, $c ) = @_;
-    
-    my $notes;
-    
-    my $dbh = $c->dbh;
-    
-    # open
-    my $sql = q~SELECT * FROM notes WHERE status = 2~; 
-    my $sth = $dbh->prepare($sql);
-    $sth->execute;
-    $notes->{open} = $sth->fetchall_arrayref({});
-    
-    # suspended
-    $sql = q~SELECT * FROM notes WHERE status = 1~; 
-    $sth = $dbh->prepare($sql);
-    $sth->execute;
-    $notes->{suspended} = $sth->fetchall_arrayref({});
-    
-    # closed
-    $sql = q~SELECT * FROM notes WHERE status = 0 ORDER BY closed_time DESC LIMIT 5~; 
-    $sth = $dbh->prepare($sql);
-    $sth->execute;
-    $notes->{closed} = $sth->fetchall_arrayref({});
-    
-    # rejected
-    $sql = q~SELECT * FROM notes WHERE status = 3 ORDER BY closed_time DESC LIMIT 5~; 
-    $sth = $dbh->prepare($sql);
-    $sth->execute;
-    $notes->{rejected} = $sth->fetchall_arrayref({});
-    
-    return $notes;
-}
-
-sub edit {
-    my ( $self, $c ) = @_;
-    
-    my $captures = $c->match->captures;
-    my $id = $captures->{id};
-    
-    my $dbh = $c->dbh;
-    
-    # get the note
-    my $sql = q~SELECT * FROM notes WHERE note_id = ?~;
-    my $sth = $dbh->prepare($sql);
-    $sth->execute( $id );
-    my $note = $sth->fetchrow_hashref;
-    
-    my $stash = {
-        template => 'notes/add.html',
-        levels   => \%levels,
+        $c->render(template => 'redirect.html', url => '/notes/');
     };
-    unless ( $c->req->method eq 'POST' ) {
-    	# pre-fulfill
-    	$stash->{fif} = {
-    		level => $note->{level},
-    		notes => $note->{note},
-    	};
-        return $c->render( $stash );
-    }
     
-    my $params = $c->req->params->to_hash;
-    
-    my $notes = $params->{notes};
-    my $level = $params->{level};
-    $sql = q~UPDATE notes SET note = ?, level = ? WHERE note_id = ?~;
-    $sth = $dbh->prepare($sql);
-    $sth->execute( $notes, $level, $id );
-    
-    $c->render(template => 'redirect.html', url => '/notes/');
-}
+    method edit ($c) {
 
-sub delete {
-    my ( $self, $c ) = @_;
-    
-    my $captures = $c->match->captures;
-    my $id = $captures->{id};
-    
-    my $dbh = $c->dbh;
-    my $sql = q~DELETE FROM notes WHERE note_id = ?~;
-    my $sth = $dbh->prepare($sql);
-    $sth->execute( $id );
-    
-    $c->render(template => 'redirect.html', url => '/notes/');
-}
+        my $captures = $c->match->captures;
+        my $id = $captures->{id};
+        
+        my $kioku = $c->kioku;
+        my $scope = $kioku->new_scope;
+        my $note  = $kioku->lookup($id);
 
-sub update {
-	my ( $self, $c ) = @_;
-	
-	my $captures = $c->match->captures;
-    my $id = $captures->{id};
-    
-    my $dbh = $c->dbh;
-    my $params = $c->req->params->to_hash;
-    
-    my $status = $params->{status};
-    my $st_val = 2;
-    foreach my $key ( keys %status ) {
-    	if ( $status{ $key } eq $status ) {
-    		$st_val = $key;
-    		last;
-    	}
-    }
-    
-    if ( $status eq 'closed' or $status eq 'rejected' ) {
-    	my $sql = q~UPDATE notes SET status = ?, closed_time = ? WHERE note_id = ?~;
-		my $sth = $dbh->prepare($sql);
-		$sth->execute( $st_val, time(), $id );
-    } else {
-		my $sql = q~UPDATE notes SET status = ? WHERE note_id = ?~;
-		my $sth = $dbh->prepare($sql);
-		$sth->execute( $st_val, $id );
-    }
-    
-    $c->render(template => 'redirect.html', url => '/notes/');
-}
+        my $stash = {
+            template => 'notes/add.html',
+        };
+        unless ( $c->req->method eq 'POST' ) {
+        	# pre-fulfill
+        	$stash->{fif} = {
+        		text => $note->text,
+        	};
+            return $c->render( $stash );
+        }
+        
+        my $params = $c->req->params->to_hash;
+        
+        $note->text( $params->{text} );
+        
+        {
+            my $scope = $kioku->new_scope;
+            $kioku->txn_do(sub {
+                $kioku->update($note);
+            });
+        }
 
-sub view_all {
-	my ( $self, $c ) = @_;
-	
-	my $dbh = $c->dbh;
-	
-	my $params = $c->req->params->to_hash;
-	my $status = $params->{status};
-    my $st_val = 0;
-    foreach my $key ( keys %status ) {
-    	if ( $status{ $key } eq $status ) {
-    		$st_val = $key;
-    		last;
-    	}
-    }
-	
-	my $sql = q~SELECT * FROM notes WHERE status = ? ORDER BY time DESC~; 
-    my $sth = $dbh->prepare($sql);
-    $sth->execute($st_val);
-    my $notes = $sth->fetchall_arrayref({});
+        $c->render(template => 'redirect.html', url => '/notes/');
+    };
     
-    $c->render(
-		template => 'notes/index.html',
-		notes => { $status => $notes },
-		is_in_view_all_page => 1,
-		status => $status,
-		levels => \%levels
-	);
-}
+    method delete ($c) {
+
+        my $captures = $c->match->captures;
+        my $id = $captures->{id};
+        
+        my $kioku = $c->kioku;
+        my $scope = $kioku->new_scope;
+        $kioku->delete($id);
+
+        $c->render(template => 'redirect.html', url => '/notes/');
+    };
+    
+    method update ($c) {
+    	
+    	my $captures = $c->match->captures;
+        my $id = $captures->{id};
+        
+        my $params = $c->req->params->to_hash;
+        
+        my $kioku = $c->kioku;
+        my $scope = $kioku->new_scope;
+        my $note  = $kioku->lookup($id);
+        
+        my $status = $params->{status};
+        if ( $status eq 'closed' or $status eq 'rejected' ) {
+            $note->status( $status );
+            $note->closed_time( time() );
+        } else {
+            $note->status( $status );
+        }
+        
+        {
+            my $scope = $kioku->new_scope;
+            $kioku->txn_do(sub {
+                $kioku->update($note);
+            });
+        }
+        
+        $c->render(template => 'redirect.html', url => '/notes/');
+    };
+    
+    method view_all ($c) {
+
+    	my $params = $c->req->params->to_hash;
+    	my $status = $params->{status};
+        
+        my $notes;
+        my $kioku = $c->kioku;
+        my $scope = $kioku->new_scope;
+        my $all = $kioku->backend->all_entries;
+        while( my $chunk = $all->next ){
+            entry: for my $id (@$chunk) {
+                my $entry = $kioku->lookup($id->id);
+                next entry unless blessed $entry && $entry->isa('DayDayUpX::Note');
+                $entry->{id} = $id->id; # hack
+                push @{ $notes }, $entry if $entry->status eq $status;
+            }
+        }
+
+        $c->render(
+    		template => 'notes/index.html',
+    		notes => { $status => $notes },
+    		is_in_view_all_page => 1,
+    		status => $status,
+    	);
+    }
+};
 
 1;
-__END__
-
-=head1 NAME
-
-DayDayUp::Notes - Mojolicious::Controller, /notes/
-
-=head1 URL
-
-	/notes/
-	/notes/add
-	/notes/$id/edit
-	/notes/$id/delete
-	/notes/$id/update
-	/notes/view_all
-
-=head1 AUTHOR
-
-Fayland Lam < fayland at gmail dot com >
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright 2008 Fayland Lam, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-=cut
---
+++ lib/DayDayUpX/Note.pm
@@ -0,0 +1,26 @@
+use MooseX::Declare;
+
+class DayDayUpX::Note {
+    use MooseX::Types::Moose qw(Str Int);
+    use Moose::Util::TypeConstraints;
+
+    
+
+    has 'text' => (
+        is  => 'rw',
+        isa => Str,
+        required => 1,
+    );
+    
+    enum 'NoteStatus' => qw(open closed rejected suspended);
+    has 'status' => (
+        is  => 'rw',
+        isa => 'NoteStatus',
+        default => 'open',
+    );
+    
+    has 'time' => ( is => 'rw', isa => Int, required => 1 );
+    has 'closed_time' => ( is => 'rw', isa => Int, default => 0 );
+};
+
+1;
