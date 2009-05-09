cf6e3ccb41e8366e9f6a93dae8da9d0d1853f05b
Fayland 林 <fayland@gmail.com>
2009-05-09 04:33:07 +0000

    Import of FAYLAND/DayDayUp-0.94 from CPAN.
    gitpan-cpan-distribution: DayDayUp
    gitpan-cpan-version:      0.94
    gitpan-cpan-path:         FAYLAND/DayDayUp-0.94.tar.gz
    gitpan-cpan-author:       FAYLAND
    gitpan-cpan-maturity:     released

--- lib/DayDayUp.pm
+++ lib/DayDayUp.pm
@@ -4,7 +4,7 @@ use MooseX::Declare;
 
 class DayDayUp extends Mojolicious is mutable {
 
-    our $VERSION = '0.93';
+    our $VERSION = '0.94';
     
     use File::Spec ();
     use Template::Stash::XS ();
@@ -52,7 +52,7 @@ class DayDayUp extends Mojolicious is mutable {
     
         # Default route
         $r->route('/:controller/:action')
-          ->to(controller => 'index', action => 'index');
+          ->to(controller => 'notes', action => 'index');
     
         my $tt = MojoX::Renderer::TT->build(
             mojo => $self,
